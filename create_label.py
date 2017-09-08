f1c86833111c751824586c856ad93cdb13002277
Jean-Christophe Perrin <jc.m.perrin@gmail.com>
2017-09-08 12:16:15 +0000

    Initialized qr code reader proejct

---
+++ create_label.py
@@ -0,0 +1,17 @@
+"""
+This script creates a qr label of the input file.
+
+@author JC Perrin
+@date 2017-09-09
+"""
+
+from PIL import Image
+import qrcode
+import sys
+
+
+def main():
+    print 'Hello, world!'
+
+if __name__=='__main__':
+    main()
